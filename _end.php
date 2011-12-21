b15ef814d9cbae7b57bc8d14ebc6cbd9e2f1891c
Garrett Winder <gw@garrettwinder.com>
2011-12-21 16:57:57 -0600

    Updated jQuery

--- _end.php
+++ _end.php
@@ -1,16 +1,16 @@
         <div id="footer" role="contentinfo">
-        
+
             <p class="copyright">&copy; <?php echo date('Y') ?> Site name</p>
             <p class="credit">Site by <a href="http://erskinedesign.com/">Erskine Design</a></p>
-        
+
         </div> <!-- // #footer -->
-    
+
     </div> <!-- // #page -->
-    
+
     <!-- JAVASCRIPT -->
     <script src="/static/js/EDJ.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
     <!-- CALL DOC READY FUNCTIONS -->
     <script>
     $(document).ready(function() {
@@ -21,11 +21,11 @@
         EDJ.on_ready(settings);
     });
     </script>
-    
+
     <!--[if IE 6]>
         <script src="/static/js/ie6/DD_belatedPNG_0.0.8a.js"></script>
         <script src="/static/js/ie6/ie6.js"></script>
     <![endif]-->
-    
+
     </body>
 </html>
