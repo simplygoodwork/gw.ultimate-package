0536ec98d92e6c895aadb15b508e0ff91ce5eefa
Garrett Winder <gw@garrettwinder.com>
2011-12-21 16:58:11 -0600

    Not going to default to screen_prototype anymore

--- _start.php
+++ _start.php
@@ -2,26 +2,26 @@
 <html lang="en" class="js-disabled">
 
     <head>
-    
+
         <!-- META -->
         <meta charset="UTF-8" />
         <meta name="description" content="" />
         <meta name="author" content="Erskine Design" />
         <link rel="author" type="text/plain" href="/humans.txt" />
         <title><?php echo $page_title ?> | Site name</title>
-        
+
         <!-- CSS -->
-        <link rel="stylesheet" media="all" type="text/css" href="/static/css/screen_prototype.css" />
+        <link rel="stylesheet" media="all" type="text/css" href="/static/css/screen.css" />
         <link rel="stylesheet" media="all" type="text/css" href="/static/css/components.css" />
-        
+
         <!-- IE CSS -->
         <!--[if IE 8]><link rel="stylesheet" media="all" type="text/css" href="/static/css/ie/screen_ie8.css" /><![endif]-->
         <!--[if lte IE 7]><link rel="stylesheet" media="all" type="text/css" href="/static/css/ie/screen_ie.css" /><![endif]-->
         <!--[if IE 7]><link rel="stylesheet" media="all" type="text/css" href="/static/css/ie/screen_ie7.css" /><![endif]-->
         <!--[if IE 6]><link rel="stylesheet" media="all" type="text/css" href="/static/css/ie/screen_ie6.css" /><![endif]-->
-        
+
         <link rel="stylesheet" media="print" type="text/css" href="/static/css/print.css" />
-        
+
         <!-- ICONS -->
         <link rel="icon" type="image/png" href="/static/images/site/icons/favicon.png" />
         <!--
@@ -30,26 +30,26 @@
         <link rel="apple-touch-icon" sizes="114x114" href="/assets/images/site/icons/touch-iphone4.png" />
         <link rel="apple-touch-startup-image" size="320x460" href="/assets/images/site/icons/touch-startup.png">
         -->
-        
+
         <!-- RSS -->
         <!-- <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="/rss/feed.xml" /> -->
-        
+
         <!-- THE EDJ -->
         <script src="/static/js/EDJ.bootstrap.js"></script>
-    
+
     </head>
-    
+
     <body class="<?php echo $body_class ?>">
-    
+
         <ul id="nav_access">
             <li><a href="#nav_pri">Skip to navigation</a></li>
             <li><a href="#content_wrapper">Skip to content</a></li>
         </ul> <!-- // #nav_access -->
-        
+
         <div id="page">
-        
+
             <div id="header" role="banner">
-            
+
                 <?php if ($location == "home") { ?> 
                     <h1 id="branding">
                         <img src="/static/images/site/branding/branding.png" alt="Your branding" width="480" height="100" />
@@ -61,7 +61,7 @@
                         </a>
                     </p> <!-- // #branding -->
                 <?php } ?> 
-                
+
                 <form action="#" id="site_search">
                     <fieldset>
                         <div>
@@ -73,13 +73,12 @@
                         </div>
                     </fieldset>
                 </form> <!-- // #site_search -->
-            
+
             </div> <!-- // #header -->
-            
+
             <ul class="horizontal" id="nav_pri" role="navigation">
                 <li<?php if ($location == "home") { ?> class="cur"<?php } ?>><a href="/">Home</a></li>
                 <li<?php if ($location == "section_1") { ?> class="cur"<?php } ?>><a href="/section1">Section 1</a></li>
                 <li<?php if ($location == "section_2") { ?> class="cur"<?php } ?>><a href="/section2">Section 2</a></li>
                 <li<?php if ($location == "section_3") { ?> class="cur"<?php } ?>><a href="/section3">Section 3</a></li>
             </ul> <!-- // #nav_pri -->
-    
