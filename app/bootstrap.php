8c4252aab113e61c6b0b626ae78d36789d180a01
Wil Linssen <hello@wil-linssen.com>
2012-07-15 21:31:03 +0100

    ditched git submodules etc. for composer and autoload

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 
-require_once __DIR__.'/../vendor/silex.phar';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../templates',
-    'twig.class_path' => __DIR__.'/../vendor/twig/lib',
     'twig.options' => array('debug' => true)
 ));
